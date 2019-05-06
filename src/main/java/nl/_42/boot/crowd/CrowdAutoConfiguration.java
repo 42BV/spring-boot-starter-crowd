9ccbca8667a7219069d2990f3fb939e344a40bd6
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2019-05-06 12:36:35 +0200

    Copied best practices from rest-secured library

--- src/main/java/nl/_42/boot/crowd/CrowdAutoConfiguration.java
+++ src/main/java/nl/_42/boot/crowd/CrowdAutoConfiguration.java
@@ -3,10 +3,12 @@ package nl._42.boot.crowd;
 import com.atlassian.crowd.integration.http.HttpAuthenticator;
 import com.atlassian.crowd.integration.springsecurity.RemoteCrowdAuthenticationProvider;
 import com.atlassian.crowd.integration.springsecurity.user.CrowdUserDetailsService;
+import com.atlassian.crowd.integration.springsecurity.user.CrowdUserDetailsServiceImpl;
 import com.atlassian.crowd.service.AuthenticationManager;
 import com.atlassian.crowd.service.GroupMembershipManager;
 import com.atlassian.crowd.service.UserManager;
 import com.atlassian.crowd.service.soap.client.SoapClientPropertiesImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -14,20 +16,21 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.core.env.Environment;
 
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toMap;
 
 /**
  * Automatically registers a CROWD authentication provider.
  */
+@Slf4j
 @Configuration
 @ConditionalOnProperty(name = "crowd.enabled", havingValue = "true", matchIfMissing = true)
 @ImportResource("classpath:/applicationContext-CrowdClient.xml")
 public class CrowdAutoConfiguration {
-
-  private static final String DEFAULT_ROLE_PREFIX = "ROLE_";
-  private static final String ROLE_PREFIX_NAME = "crowd.role_prefix";
 
   @Autowired
   private HttpAuthenticator httpAuthenticator;
@@ -43,26 +46,33 @@ public class CrowdAutoConfiguration {
   private UserManager userManager;
 
   @Bean
-  public RemoteCrowdAuthenticationProvider crowdAuthenticationProvider(Environment environment) {
-    CrowdUserDetailsService userDetailsService = crowdUserDetailsService(environment);
-    return new RemoteCrowdAuthenticationProvider(crowdAuthenticationManager, httpAuthenticator, userDetailsService);
+  public RemoteCrowdAuthenticationProvider crowdAuthenticationProvider() {
+    return new RemoteCrowdAuthenticationProvider(crowdAuthenticationManager, httpAuthenticator, crowdUserDetailsService());
   }
 
-  private CrowdUserDetailsService crowdUserDetailsService(Environment environment) {
-    CrowdRoleMapper mapper = crowdRoleMapper(environment);
+  @Bean
+  public CrowdUserDetailsService crowdUserDetailsService() {
+    CrowdUserDetailsServiceImpl crowdUserDetailsService = new CrowdUserDetailsServiceImpl();
+    crowdUserDetailsService.setGroupMembershipManager(groupMembershipManager);
+    crowdUserDetailsService.setUserManager(userManager);
 
-    CrowdUserDetailsMappingService userDetailService = new CrowdUserDetailsMappingService(mapper);
-    userDetailService.setGroupMembershipManager(groupMembershipManager);
-    userDetailService.setUserManager(userManager);
-    userDetailService.setAuthorityPrefix(""); // Included by role mapper
-    return userDetailService;
+    Set<Map.Entry<String, String>> roleMappings = getCrowdGroupToAuthorityMappings();
+    if (!roleMappings.isEmpty()) {
+      log.info("Found 'crowd.roles' in spring boot application properties.");
+      roleMappings.forEach(roleMapping -> log.info("\t {}", roleMapping));
+      crowdUserDetailsService.setGroupToAuthorityMappings(roleMappings);
+    } else {
+      log.warn("No 'crowd.roles' found in spring boot application properties, no conversion of Crowd Groups will be applied!");
+    }
+
+    return crowdUserDetailsService;
   }
 
-  private CrowdRoleMapper crowdRoleMapper(Environment environment) {
-    CrowdRoleMapper mapper = new CrowdRoleMapper();
-    mapper.setPrefix(environment.getProperty(ROLE_PREFIX_NAME, DEFAULT_ROLE_PREFIX));
-    mapper.setRoles(crowdRoles());
-    return mapper;
+  private Set<Map.Entry<String, String>> getCrowdGroupToAuthorityMappings() {
+    return crowdRoles().entrySet().stream().collect(toMap(
+        e -> e.getKey().toString(),
+        e -> e.getValue().toString()
+      )).entrySet();
   }
 
   @Bean
--- src/test/java/nl/_42/boot/crowd/ConfigurationTest.java
+++ src/test/java/nl/_42/boot/crowd/ConfigurationTest.java
@@ -1,6 +1,7 @@
 package nl._42.boot.crowd;
 
 import com.atlassian.crowd.integration.springsecurity.RemoteCrowdAuthenticationProvider;
+import com.atlassian.crowd.integration.springsecurity.user.CrowdUserDetailsServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
@@ -16,6 +21,9 @@ public class ConfigurationTest {
 
     @Autowired
     private RemoteCrowdAuthenticationProvider crowdAuthenticationProvider;
+
+    @Autowired
+    private CrowdUserDetailsServiceImpl crowdUserDetailsService;
 
     @Test
     public void loads() {
@@ -26,6 +34,18 @@ public class ConfigurationTest {
     public void login() {
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("user", "password");
         crowdAuthenticationProvider.authenticate(token);
+    }
+
+    @Test
+    public void mapping() {
+        Iterable<Map.Entry<String, String>> iterable = crowdUserDetailsService.getGroupToAuthorityMappings();
+
+        Map<String, String> mappings = new HashMap<>();
+        iterable.forEach(entry -> mappings.put(entry.getKey(), entry.getValue()));
+
+        assertEquals(2, mappings.size());
+        assertEquals("ROLE_admin", mappings.get("crowd-app-dev-admin"));
+        assertEquals("ROLE_user", mappings.get("crowd-app-dev-user"));
     }
 
 }
