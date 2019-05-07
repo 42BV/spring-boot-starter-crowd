0218dbb33293040f4867b9af5f6e632e07759b80
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2019-05-07 10:17:46 +0200

    Removed crowd xml configuration and defined custom java config, soap client properties was poluting logs with false positives and some beans are not needed

--- src/main/java/nl/_42/boot/crowd/CrowdAutoConfiguration.java
+++ src/main/java/nl/_42/boot/crowd/CrowdAutoConfiguration.java
@@ -1,25 +1,36 @@
 package nl._42.boot.crowd;
 
 import com.atlassian.crowd.integration.http.HttpAuthenticator;
+import com.atlassian.crowd.integration.http.HttpAuthenticatorImpl;
 import com.atlassian.crowd.integration.springsecurity.RemoteCrowdAuthenticationProvider;
 import com.atlassian.crowd.integration.springsecurity.user.CrowdUserDetailsService;
 import com.atlassian.crowd.integration.springsecurity.user.CrowdUserDetailsServiceImpl;
 import com.atlassian.crowd.service.AuthenticationManager;
+import com.atlassian.crowd.service.GroupManager;
 import com.atlassian.crowd.service.GroupMembershipManager;
 import com.atlassian.crowd.service.UserManager;
+import com.atlassian.crowd.service.cache.BasicCache;
+import com.atlassian.crowd.service.cache.CacheAwareAuthenticationManager;
+import com.atlassian.crowd.service.cache.CacheImpl;
+import com.atlassian.crowd.service.cache.CachingGroupManager;
+import com.atlassian.crowd.service.cache.CachingGroupMembershipManager;
+import com.atlassian.crowd.service.cache.CachingUserManager;
+import com.atlassian.crowd.service.soap.client.SecurityServerClient;
+import com.atlassian.crowd.service.soap.client.SecurityServerClientImpl;
 import com.atlassian.crowd.service.soap.client.SoapClientPropertiesImpl;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import net.sf.ehcache.CacheManager;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.io.ClassPathResource;
 
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toMap;
 
@@ -29,50 +40,37 @@ import static java.util.stream.Collectors.toMap;
 @Slf4j
 @Configuration
 @ConditionalOnProperty(name = "crowd.enabled", havingValue = "true", matchIfMissing = true)
-@ImportResource("classpath:/applicationContext-CrowdClient.xml")
 public class CrowdAutoConfiguration {
 
-  @Autowired
-  private HttpAuthenticator httpAuthenticator;
-
-  @Autowired
-  private GroupMembershipManager groupMembershipManager;
-
-  @Autowired
-  @Qualifier("crowdAuthenticationManager")
-  private AuthenticationManager crowdAuthenticationManager;
-
-  @Autowired
-  private UserManager userManager;
-
   @Bean
-  public RemoteCrowdAuthenticationProvider crowdAuthenticationProvider() {
-    return new RemoteCrowdAuthenticationProvider(crowdAuthenticationManager, httpAuthenticator, crowdUserDetailsService());
+  public RemoteCrowdAuthenticationProvider crowdAuthenticationProvider(AuthenticationManager authenticationManager, HttpAuthenticator httpAuthenticator, CrowdUserDetailsService userDetailsService) {
+    return new RemoteCrowdAuthenticationProvider(authenticationManager, httpAuthenticator, userDetailsService);
   }
 
   @Bean
-  public CrowdUserDetailsService crowdUserDetailsService() {
-    CrowdUserDetailsServiceImpl crowdUserDetailsService = new CrowdUserDetailsServiceImpl();
-    crowdUserDetailsService.setGroupMembershipManager(groupMembershipManager);
-    crowdUserDetailsService.setUserManager(userManager);
+  public CrowdUserDetailsService crowdUserDetailsService(GroupMembershipManager groupMembershipManager, UserManager userManager) {
+    CrowdUserDetailsServiceImpl userDetailsService = new CrowdUserDetailsServiceImpl();
+    userDetailsService.setGroupMembershipManager(groupMembershipManager);
+    userDetailsService.setUserManager(userManager);
+    setGroupToAuthorityMappings(userDetailsService);
+    return userDetailsService;
+  }
 
-    Set<Map.Entry<String, String>> roleMappings = getCrowdGroupToAuthorityMappings();
-    if (!roleMappings.isEmpty()) {
+  private void setGroupToAuthorityMappings(CrowdUserDetailsServiceImpl userDetailsService) {
+    Set<Map.Entry<String, String>> roles = getCrowdRolesMapping();
+
+    if (!roles.isEmpty()) {
       log.info("Found 'crowd.roles' in spring boot application properties.");
-      roleMappings.forEach(roleMapping -> log.info("\t {}", roleMapping));
-      crowdUserDetailsService.setGroupToAuthorityMappings(roleMappings);
+      roles.forEach(roleMapping -> log.info("\t {}", roleMapping));
+      userDetailsService.setGroupToAuthorityMappings(roles);
     } else {
       log.warn("No 'crowd.roles' found in spring boot application properties, no conversion of Crowd Groups will be applied!");
     }
-
-    return crowdUserDetailsService;
   }
 
-  private Set<Map.Entry<String, String>> getCrowdGroupToAuthorityMappings() {
-    return crowdRoles().entrySet().stream().collect(toMap(
-        e -> e.getKey().toString(),
-        e -> e.getValue().toString()
-      )).entrySet();
+  private Set<Map.Entry<String, String>> getCrowdRolesMapping() {
+    Stream<Map.Entry<Object, Object>> entries = crowdRoles().entrySet().stream();
+    return entries.collect(toMap(e -> e.getKey().toString(), e -> e.getValue().toString())).entrySet();
   }
 
   @Bean
@@ -87,20 +85,48 @@ public class CrowdAutoConfiguration {
     return new Properties();
   }
 
-  @Configuration
-  static class CrowdUpdatePropertiesConfig {
+  @Bean
+  public SecurityServerClient crowdSoapServerClient() {
+    SoapClientPropertiesImpl properties = SoapClientPropertiesImpl.newInstanceFromProperties(crowdProperties());
+    return new SecurityServerClientImpl(properties);
+  }
 
-    /**
-     * Sets the crowd properties defined in the application yml in the clientProperties instance of Atlassian.
-     * This way you do need the crowd.properties file in your classpath.
-     * @param crowdProperties A bean holding crowd properties of the application.yml
-     * @param clientProperties The Atlassian bean that holds the crowd properties used by their crowd authenticator.
-     */
-    @Autowired
-    CrowdUpdatePropertiesConfig(Properties crowdProperties, SoapClientPropertiesImpl clientProperties) {
-      clientProperties.updateProperties(crowdProperties);
-    }
+  @Bean
+  public AuthenticationManager crowdAuthenticationManager(SecurityServerClient securityServerClient, UserManager userManager) {
+    return new CacheAwareAuthenticationManager(securityServerClient, userManager);
+  }
 
+  @Bean
+  public HttpAuthenticator crowdAuthenticator(AuthenticationManager authenticationManager) {
+    return new HttpAuthenticatorImpl(authenticationManager);
+  }
+
+  @Bean
+  public CacheManager crowdCacheManager() {
+    EhCacheManagerFactoryBean manager = new EhCacheManagerFactoryBean();
+    manager.setConfigLocation(new ClassPathResource("crowd-ehcache.xml"));
+    manager.afterPropertiesSet();
+    return manager.getObject();
+  }
+
+  @Bean
+  public BasicCache crowdCache(CacheManager crowdCacheManager) {
+    return new CacheImpl(crowdCacheManager);
+  }
+
+  @Bean
+  public UserManager crowdUserManager(SecurityServerClient securityServerClient, BasicCache cache) {
+    return new CachingUserManager(securityServerClient, cache);
+  }
+
+  @Bean
+  public GroupManager crowdGroupManager(SecurityServerClient securityServerClient, BasicCache cache) {
+    return new CachingGroupManager(securityServerClient, cache);
+  }
+
+  @Bean
+  public GroupMembershipManager crowdGroupMembershipManager(SecurityServerClient securityServerClient, UserManager userManager, GroupManager groupManager, BasicCache cache) {
+    return new CachingGroupMembershipManager(securityServerClient, userManager, groupManager, cache);
   }
 
 }
