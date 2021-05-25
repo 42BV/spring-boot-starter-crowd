50570c36a1539e9be8a6723d91ba0b5cbdd628c0
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2021-05-25 08:16:04 +0200

    Added logging to CROWD client

--- src/main/java/nl/_42/boot/crowd/rest/CrowdClient.java
+++ src/main/java/nl/_42/boot/crowd/rest/CrowdClient.java
@@ -41,6 +41,8 @@ public class CrowdClient {
     }
 
     public String authenticate(String username, String password) throws AuthenticationException {
+        log.info("Validating CROWD password");
+
         AuthenticationContext context = new AuthenticationContext();
         context.setUsername(username);
         context.setPassword(password);
@@ -48,8 +50,11 @@ public class CrowdClient {
         try {
             String body = marshall(context);
             Session session = (Session) send("/session?validate-password=true", HttpMethod.POST, body);
+            log.info("CROWD password validated");
+
             return session.getToken();
         } catch (RestClientException rce) {
+            log.error("Could not authenticate via CROWD", rce);
             throw new AuthenticationServiceException("Could not authenticate", rce);
         }
     }
@@ -61,21 +66,26 @@ public class CrowdClient {
     }
 
     public User getUser(String username) {
+        log.info("Retrieving user");
+
         try {
             String path = String.format("/user?username=%s", username);
             return (User) send(path, HttpMethod.GET, null);
         } catch (RestClientException rce) {
-            throw new AuthenticationServiceException("Could not retrieve user", rce);
+            log.error("Could not retrieve CROWD user", rce);
+            throw new AuthenticationServiceException("Could not retrieve CROWD user", rce);
         }
     }
 
     public Set<String> getGroups(String username) {
+        log.info("Retrieving groups");
+
         try {
             String path = String.format("/user/group/nested?username=%s", username);
             Groups groups = (Groups) send(path, HttpMethod.GET, null);
             return groups.getGroups().stream().map(Group::getName).collect(Collectors.toSet());
         } catch (RestClientException rce) {
-            log.warn("Could not authenticate group", rce);
+            log.error("Could not retrieve CROWD groups", rce);
             return Collections.emptySet();
         }
     }
